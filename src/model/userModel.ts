0b506b1f3b187460205d0f1c55f64b8791d13406
amal vinod <amalvinodb2000@gmail.com>
2023-01-13 13:43:09 +0530

    started new project

---
+++ src/model/userModel.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+	name: {
+		type: String,
+		require: true,
+		unique: true,
+		dropDubs: true,
+	},
+	email: {
+		type: String,
+		require: true,
+		unique: true,
+		dropDubs: true,
+	},
+	birth: {
+		type: Date,
+		require: true,
+	},
+	phoneno: {
+		type: String,
+		require: true,
+	},
+	signup: {
+		type: Date,
+		require: true,
+	},
+	status: {
+		type: Boolean,
+		require: true,
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+	image: {
+		type: String,
+	},
+});
+
+const User = mongoose.model("User", schema);
+
+export default User;
---
+++ src/types/user.interface.ts
@@ -0,0 +1,11 @@
+interface IUser {
+	name: string | undefined;
+	email: string | undefined;
+	birth: Date | undefined;
+	phoneno: string | undefined;
+	signup: Date | undefined;
+	status: boolean | undefined;
+	password: string | undefined;
+	image: string | undefined;
+}
+export default IUser;
